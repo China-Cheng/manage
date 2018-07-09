16dc1594e1c49d79b7a641763deb147904ab972c
name-WuCheng <1014967685qq@.com>
2018-07-09 23:00:08 +0800

    登录页面完成

--- src/main.js
+++ src/main.js
@@ -5,8 +5,12 @@ import App from './App';
 import router from './router';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import '@/assets/css/style.css';
+import MyAxios from '@/plugins/MaAxios';
 
 Vue.config.productionTip = false;
+// 注册MyAxios插件
+Vue.use(MyAxios);
 // 注册element插件
 Vue.use(ElementUI);
 /* eslint-disable no-new */
