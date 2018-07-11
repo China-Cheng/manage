2fc3f18558db210b0865302c5b7479521f7ae232
name-WuCheng <1014967685qq@.com>
2018-07-11 09:49:30 +0800

    角色列表-页面搭建

--- src/views/roles/Roles.vue
+++ src/views/roles/Roles.vue
@@ -6,17 +6,61 @@
       <el-breadcrumb-item :to="{ path: '/roles' }">权限管理</el-breadcrumb-item>
       <el-breadcrumb-item>角色列表</el-breadcrumb-item>
     </el-breadcrumb>
+
+    <el-row :span="24" class="row-add">
+    <!-- 添加按钮 -->
+    <el-button>添加角色</el-button>
+    </el-row>
+
+     <!-- 表格 -->
+    <el-table
+      stripe
+      border
+      :data="list"
+      style="width: 100%">
+      <el-table-column
+      type="index"
+      width="50">
+      </el-table-column>
+
+      <el-table-column
+        prop="roleName"
+        label="角色名称"
+        width="300">
+      </el-table-column>
+
+      <el-table-column
+        prop="roleDesc"
+        label="角色描述"
+        width="300">
+      </el-table-column>
+
+      <el-table-column
+        label="操作">
+        <template slot-scope="scope">
+          <el-row>
+            <el-button @click="handleEdit(scope.row)" size="mini" type="primary" icon="el-icon-edit" circle></el-button>
+            <el-button @click="handleDelete(scope.row.id)" size="mini" type="danger" icon="el-icon-delete" circle></el-button>
+            <el-button size="mini" type="success" icon="el-icon-check" circle></el-button>
+          </el-row>
+        </template>
+      </el-table-column>
+    </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      list: []
+    };
+  }
 };
 </script>
 
 <style>
-.box-card {
-  height: 100%;
+.row-add {
+  margin: 10px 0;
 }
 </style>
