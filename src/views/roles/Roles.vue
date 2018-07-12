da03acb87a10771fbf4cca6d253fde9bc7067b8f
name-WuCheng <1014967685qq@.com>
2018-07-12 22:41:50 +0800

    角色列表-授权

--- src/views/roles/Roles.vue
+++ src/views/roles/Roles.vue
@@ -46,7 +46,7 @@
                     @close="hanldeClose(scope.row, item3.id)"
                     class="level3"
                     closable
-                    type="warning"
+                      type="warning"
                     v-for="item3 in item2.children"
                     :key="item3.id">
                     {{ item3.authName }}
@@ -80,32 +80,38 @@
         <template slot-scope="scope">
           <el-button plain size="mini" type="primary" icon="el-icon-edit" ></el-button>
           <el-button plain size="mini" type="danger" icon="el-icon-delete" ></el-button>
-          <el-button @click="dialogVisible=true" plain size="mini" type="success" icon="el-icon-check" ></el-button>
+          <el-button @click="handleShowRightsDialog(scope.row)" plain size="mini" type="success" icon="el-icon-check" ></el-button>
         </template>
       </el-table-column>
     </el-table>
 
     <!-- 分配权限的对话框 -->
     <el-dialog
-      v-loading="loadingTree"
       @open="handleOpenDialog"
       title="分配权限"
       :visible.sync="dialogVisible">
-
       <!-- 树形结构
         data: 提供树形数据
         props: 设置数据中显示的属性
+
+        node-key - 给每一个节点一个表示 ，一般绑定id
+        当要使用default-expanded-keys和default-checked-keys必须先设置node-key
+        default-checked-keys 设置默认选中的节点
        -->
       <el-tree
+        ref="tree"
+        v-loading="loadingTree"
         :data="treeData"
         :props="defaultProps"
+        node-key="id"
+        :default-checked-keys="checkedList"
         show-checkbox
         default-expand-all>
       </el-tree>
 
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="handleSetRights">确 定</el-button>
       </span>
     </el-dialog>
   </el-card>
@@ -126,7 +132,11 @@ export default {
       defaultProps: {
         children: 'children',
         label: 'authName'
-      }
+      },
+      // 获取要选择的节点的id
+      checkedList: [],
+      // 记录当前修改的角色id
+      currentRoleId: -1
     };
   },
   created() {
@@ -175,8 +185,57 @@ export default {
       const { data: resData } = await this.$http.get('rights/tree');
 
       this.loadingTree = false;
+
       const { data } = resData;
       this.treeData = data;
+    },
+    // 点击按钮，显示分配权限的对话框
+    handleShowRightsDialog(role) {
+      // 记录角色id， 分配权限的时候使用
+      this.currentRoleId = role.id;
+
+      this.dialogVisible = true;
+      // 获取当前角色所拥有的权限的id
+
+      // 遍历一级权限
+      const arr = [];
+      role.children.forEach((item1) => {
+        // arr.push(item1.id);
+        // 遍历二级权限
+        item1.children.forEach((item2) => {
+          // arr.push(item2.id);
+          // 遍历三级权限
+          item2.children.forEach((item3) => {
+            arr.push(item3.id);
+          });
+        });
+      });
+      this.checkedList = arr;
+    },
+    // 点击确定按钮，分配权限
+    async handleSetRights() {
+      // 获取到被选中的节点的id
+      const checkedKeys = this.$refs.tree.getCheckedKeys();
+      // 获取到半选的节点的id
+      const halfCheckedKeys = this.$refs.tree.getHalfCheckedKeys();
+      const newArray = [...checkedKeys, ...halfCheckedKeys];
+
+      const { data: resData } = await this.$http.post(`roles/${this.currentRoleId}/rights`, {
+        rids: newArray.join(',')
+      });
+
+      const { meta: { status, msg } } = resData;
+      if (status === 200) {
+        // 成功
+        // 关闭对话框
+        this.dialogVisible = false;
+        // 提示
+        this.$message.success(msg);
+        // 重新加载数据
+        this.loadData();
+      } else {
+        this.$message.error(msg);
+      }
     }
   }
 };
