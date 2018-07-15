4a909f23dd9e1168f3363691715f90eb05aa7be2
name-WuCheng <1014967685qq@.com>
2018-07-15 09:35:39 +0800

    图片上传

--- src/views/goods/Add.vue
+++ src/views/goods/Add.vue
@@ -43,7 +43,23 @@
         <el-button @click="handleNextStep">下一步</el-button>
       </el-tab-pane>
       <el-tab-pane label="商品图片" name="1">
-        商品图片
+        <!-- action:用来指定图片上传的地址，只要告诉他图片上传的接口即可
+        但是他和axios没关系，baseURL没用,所以要写完整的
+        on-preview：预览图片的时候触发
+        on-remov：删除的时候触发
+        file-list：储存上传的图片数据
+        由于上传组件不是使用的axios发送的请求，所以配置的请求拦截器无效，就没有token
+        要自己配置，使用自带的Headers -->
+        <el-upload
+          action="http://localhost:8888/api/private/v1/upload"
+          :on-preview="handlePreview"
+          :on-remove="handleRemove"
+          :headers="header"
+          :file-list="fileList2"
+          list-type="picture">
+          <el-button size="small" type="primary">点击上传</el-button>
+          <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+        </el-upload>
         <el-row>
           <el-col :span="4">
             <el-button @click="handleNextStep">下一步</el-button>
@@ -87,7 +103,10 @@ export default {
         goods_introduce: ''
       },
       activeName: '0',
-      stepActive: 0
+      stepActive: 0,
+      // 图片上传
+      headers:{Authorization: window.sessionStorage.getItem('token')},
+      fileList2: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
     };
   },
   methods: {
@@ -97,17 +116,17 @@ export default {
         method: 'post',
         data: this.form
       });
-      const { data, meta } = res.data
+      const { meta } = res.data;
       if (meta.status === 201) { // 添加成功
         this.$message({
           type: 'success',
           message: meta.msg
-        })
+        });
       } else if (meta.status === 400) {
         this.$message({
           type: 'warning',
           message: meta.msg
-        })
+        });
       }
     },
     handleGaiBianLe(data) {
@@ -115,25 +134,30 @@ export default {
       this.form.goods_cat = data.join(',');
     },
     onEditorBlur () {
-      console.log('onEditorBlur')
+      console.log('onEditorBlur');
     },
     onEditorFocus () {
-      console.log('onEditorFocus')
+      console.log('onEditorFocus');
     },
     onEditorReady () {
-      console.log('onEditorReady')
+      console.log('onEditorReady');
     },
     handleNextStep () {
-      this.activeName = Number.parseInt(this.activeName) + 1 + ''
-      this.stepActive++
+      this.activeName = Number.parseInt(this.activeName) + 1 + '';
+      this.stepActive++;
     },
-    /**
-     * 处理 tabs 标签点击事件
-     */
+      // 处理 tabs 标签点击事件
     handleTabClick (tab, event) {
       // console.log('handleTabClick')
       // console.log(tab.index)
-      this.stepActive = tab.index - 0
+      this.stepActive = tab.index - 0;
+    },
+    // 图片上传
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePreview(file) {
+      console.log(file);
     }
   },
   components: {
